9c2140633b1e07b276f95509d285f040984c10d6
secorp <sem.koldewijn@student.hu.nl>
2024-10-08 11:43:31 +0200

    testing the test check

--- __tests__/test_get_questing.js
+++ __tests__/test_get_questing.js
@@ -38,7 +38,5 @@ describe('StringChecker', () => {
 
         expect(QUESTION_FIELD.textContent).toMatch(/\S+/);
     });
-    test('always passes (test check)', () => {
-        expect(true).toBe(true);
-    });
+
 });
